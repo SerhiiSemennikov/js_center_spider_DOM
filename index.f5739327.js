336bb4a0c60e0094460956b8ad96242dda043ab8
SerhiiSemennikov <semennik@hotmail.com>
2024-10-06 19:33:06 +0200

    make build

---
+++ index.f5739327.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=getComputedStyle(e),o=getComputedStyle(t),r=(n(c.width)-n(o.width))/2,l=(n(c.height)-n(o.height))/2;function n(e){return Number(e.replace("px",""))}t.style.cssText="\ntop:".concat(r,"px;\nleft:").concat(l,"px;");
+//# sourceMappingURL=index.f5739327.js.map
